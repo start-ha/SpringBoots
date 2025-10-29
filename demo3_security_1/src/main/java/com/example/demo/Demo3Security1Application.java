da68b99bf9c4d55bc7d84f9cb97a620bd0e9c6ac
hayeong <bestdev.hy@gmail.com>
2025-10-29 20:04:55 +0900

    demo_security_1

---
+++ demo3_security_1/src/main/java/com/example/demo/Demo3Security1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3Security1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3Security1Application.class, args);
+	}
+
+}
---
+++ demo3_security_1/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping
+	public String index() {
+		log.info("[[[ /admin  ]]]");
+		return "/admin/index";
+	}
+}
