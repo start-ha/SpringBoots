c74af18ea47f4144b9a8a3fe9fb9864d1f3a2c02
start-ha <hagreen0@naver.com>
2025-10-29 20:06:06 +0900

    dem03_security_2

---
+++ demo3_security_2/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping({"","/"})
+	public String home(Model model , Principal principal) {
+		
+		String loginId = principal != null ? principal.getName() : "guest";
+		model.addAttribute("loginId", loginId);
+		
+		return "index";
+	}
+}
